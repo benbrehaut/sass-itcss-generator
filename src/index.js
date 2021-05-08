c555781f3f162c7357ce945a29ca2c5db1acac4b
Ben Brehaut <bencbrehaut@gmail.com>
2021-05-08 23:13:39 +0100

    feat: cleanup of index.js

--- src/index.js
+++ src/index.js
@@ -12,13 +12,15 @@ if (!name) {
     throw new Error('❌ Please include the directory name');
 }
 
+// Create the folders and files
 init(name);
 
+// Console out completion
 console.log(`✅ Completed`);
 
 /**
  * Initialise the function
- * @param {string} folderName - the folder name to output to
+ * @param {string} folderName - the folder name to output to, comes from cli
  */
 function init(folderName) {
     // If folder already exists, throw error.
@@ -42,31 +44,38 @@ function init(folderName) {
     createIndexFile(folderName);
 }
 
+/**
+ * Create the root folder which contains all of the sub folders
+ * @param {String} rootFolder - The name of the root folder
+ */
 function createRootFolder(rootFolder) {
     fs.mkdir(rootFolder, { recursive: true }, (errorHandler));
 }
 
+/**
+ * Create a folder
+ * @param {String} rootFolder - The name of the root folder that contains the folder
+ * @param {String} folder - The name of the folder
+ */
 function createFolder(rootFolder, folder) {
     fs.mkdirSync(`${rootFolder}/${folder}`);
     fs.writeFile(`${rootFolder}/${folder}/_index.scss`, '', errorHandler);  
 }
 
+/**
+ * Create the index Sass file that are in each folder
+ * @param {String} rootFolder - The folder where the file should be created in
+ */
 function createIndexFile(rootFolder) {
     fs.copyFile(path.resolve(__dirname, './templates/_index.scss'), `./${rootFolder}/_index.scss`, errorHandler);
 }
 
+/**
+ * Handle any errors that come from trying to create the folders and files
+ * @param {Error} error - The error that comes from Node
+ */
 function errorHandler(error) {
     if (error) {
         throw error;
     }
 }
-
-// Create folders inside directory
-const createSubFolders = new Promise((resolve, reject) => {
-});
-
-// createSubFolders
-//     .then(() => {
-//     });
-    
-
