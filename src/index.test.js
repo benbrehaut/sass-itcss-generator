c112288f5f10e613757bc4ddc80aca34fa30b7e7
Ben Brehaut <bencbrehaut@gmail.com>
2021-05-11 00:34:52 +0100

    chore: adding jest testing

---
+++ src/index.test.js
@@ -0,0 +1,37 @@
+const exec = require("child_process").exec;
+const del = require("del");
+
+describe('Running the command', () => {
+    it('Should fail if no name parameter is not passed', async () => {
+        const data =  await runModule('', '.');
+
+        expect(data.code).toBe(1);
+    });
+
+    it('Should create the directory without errors', async () => {
+        const TEST_FOLDER_NAME = 'jest_test';
+        const data =  await runModule(TEST_FOLDER_NAME);
+
+        expect(data.code).toBe(0);
+        expect(data.stdout).toContain(TEST_FOLDER_NAME);
+        del.sync(TEST_FOLDER_NAME);
+    });
+});
+
+/**
+ * Run sass-itcss-generator through node
+ * @param {String} folderName - Arguments from the cli
+ * @param {*} cwd - Any options to pass to the exec command 
+ */
+function runModule(folderName, cwd) {
+    return new Promise((resolve) => {
+        exec(`node ./src/index.js ${folderName}`, { cwd }, (error, stdout, stderr) => {
+            resolve({
+                code: error && error.code ? error.code : 0,
+                error,
+                stdout,
+                stderr
+            });
+        })
+    })
+}
